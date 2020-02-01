08d6edb0fa191b43c9ff293c1413acfa9bfb4f02
Isabel B <isabelbolger@hotmail.com>
2020-01-31 22:09:44 -0500

    general ui

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  NavDropdown,
+  MenuItem,
+  Container,
+  Form,
+  FormControl,
+  Button
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Tech Together Boston</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">About Us</Nav.Link>
+              <Nav.Link href="#link">Story</Nav.Link>
+            </Nav>
+            
+          </Navbar.Collapse>
+        </Navbar>
+        <h1>German Grammer Checker</h1>
+        <form>
+            <textarea type="text" name="name" rows="10" cols="100"/>
+        </form>
+        <input type="submit" value="Submit"/>
+      </div>
+    );
+  }
 }
 
 export default App;
