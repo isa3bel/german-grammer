94312f9df4a918b4e667c5a5c216335f39a20367
Isabel B <isabelbolger@hotmail.com>
2020-02-01 00:34:19 -0500

    added css, first day

--- app/src/App.js
+++ app/src/App.js
@@ -14,13 +14,17 @@ import {
   Button
 } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import $ from 'jquery';
+import Tree from "./images/tree.png";
 
 class App extends React.Component {
+
   render() {
     return (
       <div className="App">
         <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">Tech Together Boston</Navbar.Brand>
+          <Navbar.Brand href="#home"><img src={Tree} width="70"
+          height="70"/></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
@@ -31,9 +35,16 @@ class App extends React.Component {
           </Navbar.Collapse>
         </Navbar>
         <h1>German Grammer Checker</h1>
+        <div className="row justify-content-center">
         <form>
-            <textarea type="text" name="name" rows="10" cols="100"/>
+            <p>Input</p>
+            <textarea type="text" id="inputText" name="name" rows="10" cols="50" style={{"border-radius": "10px"}}/>
         </form>
+        <form>
+            <p>Grammer result</p>
+            <textarea type="text" id="readonly" spellcheck="true" readOnly name="name" rows="10" cols="50" style={{"border-radius": "10px"}}/>
+        </form>
+        </div>
         <input type="submit" value="Submit"/>
       </div>
     );
