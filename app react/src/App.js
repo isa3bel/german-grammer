6edca40fca9446eef87639a07d4579feb110a4b2
Isabel B <isabelbolger@hotmail.com>
2020-02-02 00:32:46 -0500

    works

--- app react/src/App.js
+++ app react/src/App.js
@@ -28,30 +28,30 @@ class App extends React.Component {
       formData: {
         data: "test",
       },
-      readonlyValue:'',
       result: '',
     }
   }
 
   submitHappened = (event) => {
-    const formData = this.state.formData;
-    this.setState({ isLoading: true });
-    fetch('http://localhost:5000/#home/', 
-        {
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        method: 'POST',
-        body: JSON.stringify(formData)
-      })
-      .then(response => response.json())
-      .then(response => {
-        this.setState({
-          result: response.result,
-        });
-      });
-      $('#readonly').val(this.state.result);
+    // const formData = this.state.formData;
+    // this.setState({ isLoading: true });
+    // fetch('http://localhost:5000/#home/', 
+    //     {
+    //       headers: {
+    //         'Accept': 'application/json',
+    //         'Content-Type': 'application/json',
+    //         'Access-Control-Allow-Origin':'*',
+    //     },
+    //     method: 'POST',
+    //     body: JSON.stringify(formData)
+    //   })
+    //   .then(response => response.json())
+    //   .then(response => {
+    //     this.setState({
+    //       result: response.result,
+    //     });
+    //   });
+      $('#readonly').val(this.state.formData.data);
   }
 
   handleChange(event) {
@@ -61,6 +61,8 @@ class App extends React.Component {
     this.setState({
       formData
     });
+
+    
   }  
     
   
@@ -84,11 +86,11 @@ class App extends React.Component {
         <div className="row justify-content-center">
         <form>
             <p>Input</p>
-            <textarea type="text" id="inputText" onChange={this.handleChange.bind(this)} name="name" rows="10" cols="50" style={{"border-radius": "10px"}}/>
+            <textarea type="text" id="inputText" onChange={this.handleChange.bind(this)} name="name" rows="10" cols="50" style={{"borderRadius": "10px"}}/>
         </form>
         <form>
             <p>Grammer result</p>
-            <textarea type="text" id="readonly" value={this.state.readonlyValue} spellcheck="true" readOnly name="name" rows="10" cols="50" style={{"border-radius": "10px"}}/>
+            <textarea type="text" id="readonly" value={this.state.result} spellCheck="true" readOnly name="name" rows="10" cols="50" style={{"border-radius": "10px"}}/>
         </form>
         </div>
         <div>
